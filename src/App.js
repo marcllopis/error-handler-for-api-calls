b4e6f0e0db057419d2d6c97afef1390619726df0
Marc <marcllopis89@gmail.com>
2020-02-17 11:54:44 +0100

    Adds the error handler for api calls

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function handleError(error) {
+  return error.toString()
 }
+
+
+class App extends Component {
+  state = {
+    info: {},
+    error: ''
+  }
+  // with FETCH
+  // componentDidMount() {
+  //   fetch('https://api.github.com/users/marclloasdasdpis')
+  //     .then((response) => {
+  //       console.log(response)
+  //       // You need to "force" an error on the fetch since any status code
+  //       // is rendered as ok. If the response is not ok, we throw an error
+  //       if (!response.ok) {
+  //         throw new Error(`There was a problem with your request. Error code: ${response.status}, ${response.statusText}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(data => this.setState({info:data}))
+  //     .catch((error) => {
+  //       let errorString = `Your fetch request didn't work, it returned the following: ${error.toString()}`
+  //       this.setState({
+  //         error: errorString
+  //       });
+  //     });
+  // }
+
+  // WITH AXIOS
+  async componentDidMount() {
+    // 1st way with Try and Catch
+    // try {
+    //   const {data} = await axios('https://api.github.com/users/marcasdallopis')
+    //   this.setState({info: data})
+    // }
+    // catch(error) {
+    //   let errorString = `Your fetch request didn't work, it returned the following: ${error.toString()}`
+    //   this.setState({
+    //     error: errorString
+    //   });
+    // }
+    // ---------------------------
+    // 2n way just with a .catch after axios call
+    const data = await
+      axios('https://api.github.com/users/marsdsdcllopis').catch(error =>
+        this.setState({
+          error: handleError(error)
+        })
+      )
+    if (data) {
+      this.setState({
+        info: data.data
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        {
+          this.state.error
+            ? this.state.error
+            : this.state.info.name
+        }
+      </div >
+    );
+  }
+}
+
 
 export default App;
